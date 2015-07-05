e0221f60cd578bf02b6c7b1da8fcb65e6af18ad6
Myka Angelica Pescasio <myka.angie@gmail.com>
2015-07-05 17:08:39 +0800

    Back-up copy of the current progress:
    1. Load-Out (Main Activity)
    2. Log-In Activity
    3. Guest Home and Trainee Home
    	- includes custom toolbar
    	- includes hamburger (home) icon for opening the side navigation
    	- includes specified navigation drawer for each user (no texts yet but with image for the Trainee user)
    4. Proper Activity Redirection (atleast for the current Activities available)

---
+++ EskwelaniSARAi_v1.1/app/src/main/java/com/example/myka/eskwelanisarai/GuestDrawerFragment.java
@@ -0,0 +1,89 @@
+package com.example.myka.eskwelanisarai;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class GuestDrawerFragment extends Fragment {
+
+    public static final String PRE_FILE_NAME = "testpref";
+    public static final String KEY_USER_LEARNED_DRAWER = "user_learned_drawer";
+
+    private ActionBarDrawerToggle mDrawerToggle;
+    private DrawerLayout mDrawerLayout;
+
+    private View containerView;
+
+    //Indicate if drawer is started for the first time or is it coming from a rotation
+    private boolean mFromSavedInstanceState;
+
+    public GuestDrawerFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if(savedInstanceState == null){
+            mFromSavedInstanceState = true;
+        }
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_guest_drawer, container, false);
+    }
+
+    public void setUp (int fragmentId, DrawerLayout drawerLayout, final Toolbar toolbar) {
+        containerView = getActivity().findViewById(fragmentId);
+        mDrawerLayout = drawerLayout;
+        mDrawerToggle = new ActionBarDrawerToggle(getActivity(), drawerLayout, toolbar, R.string.drawer_open, R.string.drawer_close){
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                getActivity().invalidateOptionsMenu();
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+                getActivity().invalidateOptionsMenu();
+            }
+
+            @Override
+            public void onDrawerSlide(View drawerView, float slideOffset) {
+                if(slideOffset > -0.6){
+                    toolbar.setAlpha(1+slideOffset);
+                }
+            }
+        };
+
+        if(!mFromSavedInstanceState){
+            mDrawerLayout.openDrawer(containerView);
+        }
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+
+        mDrawerLayout.post(new Runnable() {
+            @Override
+            public void run() {
+                mDrawerToggle.syncState();
+            }
+        });
+    }
+
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # eSARAi
 Eskwela ni SARAi
+This project is a part of the Project SARAi
+This module aim to create a mobile support for a Massive Open Online Course (MOOC) model of Eskwela ni SARAi.
